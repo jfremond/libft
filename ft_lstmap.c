047d30dbe532fd0aa47064a897c88438b77d492f
jfremond <jfremond@student.42.fr>
2020-10-31 16:22:39 +0100

    Adding ft_lstmap.c

---
+++ ft_lstmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/09 11:16:20 by jfremond          #+#    #+#             */
+/*   Updated: 2020/10/31 15:47:33 by jfremond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*d)(void *))
+{
+	t_list	*elem;
+	t_list	*tmp;
+
+	elem = NULL;
+	while (lst)
+	{
+		tmp = ft_lstnew(lst->content);
+		if (!tmp)
+		{
+			ft_lstclear(&elem, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&elem, tmp);
+		lst = lst->next;
+	}
+	return (elem);
+}
