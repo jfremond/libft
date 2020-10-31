8831fe1e9165a794922462bf5d58605ab4906a77
jfremond <jfremond@student.42.fr>
2020-10-31 16:52:43 +0100

    Adding ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/29 21:24:14 by jfremond          #+#    #+#             */
+/*   Updated: 2020/10/09 17:01:20 by jfremond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	len;
+	unsigned int	i;
+	char			*res;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	res = (char *)malloc(sizeof(*res) * (len + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		res[i] = (*f)(i, s[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
