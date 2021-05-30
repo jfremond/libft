ce90674f94b4a4e00028130eb0c368af7130eba0
jfremond <jfremond@student.42.fr>
2021-05-30 11:20:49 +0200

    Fixing ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/09 11:16:20 by jfremond          #+#    #+#             */
-/*   Updated: 2020/10/31 15:47:33 by jfremond         ###   ########.fr       */
+/*   Updated: 2021/05/30 11:06:52 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*d)(void *))
 	t_list	*tmp;
 
 	elem = NULL;
+	if (!lst || !f)
+		return (NULL);
 	while (lst)
 	{
-		tmp = ft_lstnew(lst->content);
+		tmp = ft_lstnew(f(lst->content));
 		if (!tmp)
 		{
 			ft_lstclear(&elem, d);
